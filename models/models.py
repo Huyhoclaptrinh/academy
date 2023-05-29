9cb5acf398f46e35b00c25c1b00eea651268065a
Huyhoclaptrinh <giahuyphung282@gmail.com>
2023-05-29 18:59:21 +0700

    Test

--- models/models.py
+++ models/models.py
@@ -19,3 +19,29 @@ class Course(models.Model):
 
     name = fields.Char()
     teacher_id = fields.Many2one('academy.teacher', string="Teacher")
+    lesson_ids = fields.One2many('academy.lesson', 'course_id', string="Lessons")
+    state = fields.Selection([
+        ('draft', 'Draft'),
+        ('done', 'Completed'),
+        ('cancel', 'Cancelled'), ], 'State', default='draft',
+        store=True)
+
+    def do_print_picking(self):
+        self.write({'printed': True})
+        return self.env.ref('academy.action_report_picking').report_action(self)
+
+
+class Lesson(models.Model):
+    _name = 'academy.lesson'
+    _description = 'encapsulate lessons information in academy'
+
+    name = fields.Char()
+    predict_amount_hours = fields.Integer()
+    course_video = fields.Char()
+    referances = fields.Binary(string='Upload References')
+    homework = fields.Binary(string='Upload homework')
+    course_id = fields.Many2one('product.template', string="Course")
+
+
+
+
